e91dccc59e75a04c1e8bf68809d5bca81141cd67
salavudeenH <salavudeenhadji@gmail.com>
2021-07-01 17:26:35 +0200

    get element

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert,Image } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert,Image,ScrollView } from 'react-native';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 const Separator = () => (
@@ -9,22 +9,47 @@ const Separator = () => (
 const App = () =>{
   const [photoCamera,setPhotoCamera] = useState(null)
   const [photoLibrary,setPhotoLibrary] = useState(null)
-  
+  const [texte,setTexte] = useState('')
+
   const openCamera =  () =>{
     launchCamera({base64:true,mediaType:'photo',quality:1,includeBase64:true,saveToPhotos:true,cancelButtonTitle: "Annuler",maxWidth:1000,maxHeight:600},(response) => {
-      // console.log(response)
-      setPhotoCamera(response.uri)
+      console.log(response.assets[0].base64)
+      setPhotoCamera(response.assets[0].uri)
+      detectText(response.assets[0].base64)
     })
   }
+ 
   const openLibrary =  () =>{
     launchImageLibrary({},(response) => {
       console.log(response)
-     setPhotoLibrary(response.uri)
+     setPhotoLibrary(response.assets[0].uri)
     })
   }
-  
+
+  const detectText = (base64) => {
+    fetch("https://vision.googleapis.com/v1/images:annotate?key=" + "AIzaSyBz4Z44pTS0fHLYyZ7W1_Cs-Lv6zvjB138", {
+        method: 'POST',
+        body: JSON.stringify({
+          "requests": [{
+            "image": { "content": base64 },
+            "features": [
+                { "type": "TEXT_DETECTION" }
+            ]}]
+      })
+    })
+    .then(response => { return response.json()})
+    .then(jsonRes => {    
+      let text = jsonRes.responses[0].fullTextAnnotation.text
+      // navigation.navigate('ContactScreen', { text: text })
+      console.log(text)
+      setTexte(text)
+    }).catch(err => {
+      console.log('Error', err)
+    })
+  }
 
   return (
+<ScrollView>
 <SafeAreaView style={styles.container}>
         <View>
           <Text style={styles.title}>
@@ -47,11 +72,13 @@ const App = () =>{
           />
         </View>
         <Separator />
+        <Text>{texte}</Text>
         <Separator />
     <View>
-    <Image source={{uri:photoLibrary}} style={{width:480,height:480}}></Image>
+    <Image source={{uri:photoCamera}} style={{width:480,height:480}}></Image>
     </View>
-      </SafeAreaView>          
+      </SafeAreaView>      
+      </ScrollView>    
   )};
   const styles = StyleSheet.create({
     container: {
