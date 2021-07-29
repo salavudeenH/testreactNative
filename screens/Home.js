c50369b772138d1ea93bc849dbb552caeaa36ca4
MelvynMercier <43468212+MelvynMercier@users.noreply.github.com>
2021-07-29 15:10:38 +0200

    commit

--- screens/Home.js
+++ screens/Home.js
@@ -112,17 +112,11 @@ const Home = ({ navigation }) => {
         //On récupère le montant total ht de la facture
         invoice.totalHT = FindTotalHTAmount(result);
 
-        //On récupère les détails de la facture;
-        FindDetails(result);
+        //On récupère tous les détails de la facture;
+        let details = FindDetails(result);
 
-        //let resultDescription = jsonRes.responses[0].textAnnotations
-        //invoice.title = resultDescription[1].description 
-        //setTitle(invoice.title);
-
-        //let Date = result.match(/\d{2}([\/.-])\d{2}\1\d{4}/g);
-        //setDate(Date);
-
-        // console.log(invoice)
+        //On récupère la liste des en-tête et la liste des détails
+        invoice.details = FilterDetails(details);
 
         //On assigne les données trouvées
         setTotal(invoice.total);
@@ -174,15 +168,64 @@ const Home = ({ navigation }) => {
 
     //Méthode permettant de récupérer les détails
     function FindDetails(result) {
+      //On cherche le montant total ht
       let indexHT = result.search(totalHTRegexp);
+      //On cherche les dates sur la facture
       let dates = result.match(dateRegexp);
+      //On récupère la dernière date de la facture
       let lastDate = dates[dates.length - 1];
+      //On récupère l'index de la dernière date de la facture
       let indexLastDate = result.search(lastDate);
+      //On récupère ensuite tous les détails de la facture
       let allDetails = result.substring(indexLastDate, indexHT - 1);
+      //On transforme le résultat en tableau
+      let detailsArray = allDetails.split(/\r?\n|\r/g);
+      //On enlève la date placée en première
+      detailsArray.shift()
+      
+      return detailsArray;
+    }
 
-      let arrayDetails = allDetails.split(/\r?\n|\r/g);
+    //On filtre les détails
+    function FilterDetails(details){
+      let headers = new Array(0);
+      let data = new Array(0);
+      let line;
 
-      console.log(arrayDetails);
+      let endHeader = false;
+      
+      for (let i = 0; i < details.length - 1; i++) {
+        //On récupère la valeur à analyser
+        let value = details[i];
+        //Si la valeur n'est pas un entier et que les headers ne sont pas terminé
+        if(isNaN(parseInt(value)) &&  endHeader == false){
+          //On ajoute le header à la liste des headers
+          headers.push(value);
+        }
+        else{
+          //On passe la variable des header à TRUE afin de définir la fin des headers
+          endHeader = true;
+          //Si l'index modulo le nombre de header == 0
+          if(i % headers.length == 0){
+            //Si la n'est pas undefined
+            if(line != undefined)
+              //On ajoute la ligne au tableau de données
+              data.push(line)
+            //On créé une nouvelle ligne
+            line = new Array(0);
+          }
+          //On ajoute la valeur à la ligne
+          line.push(value);
+        }
+      }
+      //On ajoute la dernière ligne au tableau de données
+      data.push(line);
+      //On créé la valeur de retour
+      let result = new Array(2);
+      //[0] = en-têtes; [1] = données récupérés
+      result[0] = headers;
+      result[1] = data;
+      return result;
     }
 
     const dataList = [{ key: '1' }, { key: '2' }, { key: '3' }, { key: '4' }, { key: '5' }];
--- screens/class/invoice.js
+++ screens/class/invoice.js
@@ -2,7 +2,6 @@ export class Invoice{
     title;
     total;
     totalHT;
-    detailsTitles;
-    detailsList;
+    details;
     tva;
 }
